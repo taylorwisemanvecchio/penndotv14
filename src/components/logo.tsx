77817dc59b7182196d4de64df350f22de498d016
taylorwisemanvecchio <vecchioj@taylorwiseman.com>
2025-03-05 07:55:56 -0500

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,24 +1,10 @@
-import Image from 'next/image';
-import type { StaticImageData } from 'next/image';
-import placeholder from '@/../public/placeholder.svg';
-
-export function Logo({
-  src = placeholder,
-}: {
-  src?: StaticImageData | string;
-}) {
+export function Logo() {
   return (
-    <figure className='w-24 h-24 mx-auto border relative overflow-hidden rounded-full'>
-      <Image
-        src={src}
-        alt='Logo'
-        fill
-        sizes='96px'
-        style={{
-          objectFit: 'cover',
-        }}
-        priority
-        quality={85}
+    <figure className="w-100 h-100 mx-auto relative overflow-hidden rounded-full">
+      <img
+        src="/logo.webp" 
+        alt="Company Logo"
+        className="object-cover w-100 h-100"
       />
     </figure>
   );
