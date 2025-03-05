10e7cfde9a00f9c8d49d76a829d6f7616ec33408
taylorwisemanvecchio <vecchioj@taylorwiseman.com>
2025-03-05 07:49:10 -0500

    Update page.tsx

--- src/app/(unauthenticated)/signin/page.tsx
+++ src/app/(unauthenticated)/signin/page.tsx
@@ -1,12 +1,17 @@
+import Image from 'next/image';
 import logo from '@/../public/logo.webp';
-import { Logo } from '@/components/logo';
 import { SignInForm } from '@/components/sign-in-form';
 
 export default function SignInPage() {
   return (
-    <main aria-label='Sign in page'>
-      <Logo src={logo} />
-      <h1>Welcome to Next-14-Azure-Auth</h1>
+    <main 
+      aria-label="Sign in page"
+      className="flex flex-col items-center justify-center min-h-screen text-center space-y-6"
+    >
+      {/* Use next/image for optimization */}
+      <Image src={logo} alt="Wiseman AI Logo" width={500} height={150} priority />
+
+      <h1 className="text-2xl font-semibold">Welcome to Wiseman AI!</h1>
       <SignInForm />
     </main>
   );
